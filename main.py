e9a2ac68a1d4d468ffbdc441c91e2cbbfda87ce6
admin <kattubadi426@gmail.com>
2024-12-09 08:15:06 +0530

    Added news tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import yaml
 
-with open('/Users/admin/PycharmProjects/pytest_framework/tests/table1/config.yml','r') as f:
-    config = yaml.safe_load(f)
-    print(config)
+
+with open('/Users/admin/PycharmProjects/taf/tests/table3/config.yml','r') as f:
+    config_data = yaml.safe_load(f)
+    print("source config", config_data['source'])
+    print("target config", config_data['target'])
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 import pytest
+import yaml
 
 @pytest.fixture(scope='session')
 def spark_session():
@@ -7,9 +8,21 @@ def spark_session():
     return spark
 
 @pytest.fixture(scope='module')
-def read_data(spark_session):
+def read_config(request):
+    print("request.node.fspath.dirname", request.node.fspath.dirname)
+    dir_path = request.node.fspath.dirname
+    config_path = dir_path + '/config.yml'
+    with open(config_path, 'r') as f:
+        config_data = yaml.safe_load(f)
+    return config_data
+
+@pytest.fixture(scope='module')
+def read_data(spark_session, read_config):
+    config_data = read_config
+    source_config = config_data['source']
+    target_config = config_data['target']
     spark = spark_session
-    source = spark.read.csv("/Users/admin/PycharmProjects/taf/input_files/Contact_info_s.csv", header=True, inferSchema=True)
-    target = spark.read.csv("/Users/admin/PycharmProjects/taf/input_files/Contact_info_t.csv", header=True, inferSchema=True)
+    source = spark.read.csv(source_config['path'], header= source_config['options']['header'],inferSchema=True)
+    target = spark.read.csv(target_config['path'], header=target_config['options']['header'], inferSchema=True)
     return source, target
 

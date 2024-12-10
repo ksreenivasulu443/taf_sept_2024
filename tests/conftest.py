80dac6f1089827dbfe9a373996ad860343a5d0b1
admin <kattubadi426@gmail.com>
2024-12-10 07:26:00 +0530

    Added news tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,28 +16,25 @@ def read_config(request):
         config_data = yaml.safe_load(f)
     return config_data
 
+def read_file(config_data,spark):
+    if config_data['type'] == 'csv':
+        df = spark.read.csv(config_data['path'], header= config_data['options']['header'],inferSchema=True)
+    elif config_data['type'] == 'json':
+        df = spark.read.json(config_data['path'], multiLine=config_data['options']['multiline'] )
+    elif config_data['type'] == 'parquet':
+        df = spark.read.parquet(config_data['path'])
+    elif config_data['type'] == 'avro':
+        df = spark.read.format('avro').load(config_data['path'])
+    return df
+
 @pytest.fixture(scope='module')
-def read_data(spark_session, read_config):
+def read_data(read_config,spark_session):
+    spark = spark_session
     config_data = read_config
     source_config = config_data['source']
     target_config = config_data['target']
-    spark = spark_session
-    if source_config['type'] == 'csv':
-        source = spark.read.csv(source_config['path'], header= source_config['options']['header'],inferSchema=True)
-    elif source_config['type'] == 'json':
-        source = spark.read.json(source_config['path'], multiLine=source_config['options']['multiline'] )
-    elif source_config['type'] == 'parquet':
-        source = spark.read.parquet(source_config['path'])
-    elif source_config['type'] == 'avro':
-        source = spark.read.format('avro').load(source_config['path'])
+    source = read_file(config_data = source_config,spark=spark)
+    target = read_file(config_data =target_config,spark=spark)
 
-    if target_config['type'] == 'csv':
-        target = spark.read.csv(target_config['path'], header= target_config['options']['header'],inferSchema=True)
-    elif target_config['type'] == 'json':
-        target = spark.read.json(target_config['path'], multiLine=target_config['options']['multiline'] )
-    elif target_config['type'] == 'parquet':
-        target = spark.read.parquet(target_config['path'])
-    elif target_config['type'] == 'avro':
-        target = spark.read.format('avro').load(target_config['path'])
     return source, target
 

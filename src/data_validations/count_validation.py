7372f8c560745d4936647b655227c7e47c46d9fd
admin <kattubadi426@gmail.com>
2024-12-10 07:16:16 +0530

    Added news tests

--- src/data_validations/count_validation.py
+++ src/data_validations/count_validation.py
@@ -1,6 +1,7 @@
 def count_check(source, target):
     src_count = source.count()
     tgt_count = target.count()
+    print(f"source count is {src_count} and target count is {tgt_count}")
     if src_count == tgt_count:
         status = 'PASS'
     else:
--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,22 @@ def read_data(spark_session, read_config):
     source_config = config_data['source']
     target_config = config_data['target']
     spark = spark_session
-    source = spark.read.csv(source_config['path'], header= source_config['options']['header'],inferSchema=True)
-    target = spark.read.csv(target_config['path'], header=target_config['options']['header'], inferSchema=True)
+    if source_config['type'] == 'csv':
+        source = spark.read.csv(source_config['path'], header= source_config['options']['header'],inferSchema=True)
+    elif source_config['type'] == 'json':
+        source = spark.read.json(source_config['path'], multiLine=source_config['options']['multiline'] )
+    elif source_config['type'] == 'parquet':
+        source = spark.read.parquet(source_config['path'])
+    elif source_config['type'] == 'avro':
+        source = spark.read.format('avro').load(source_config['path'])
+
+    if target_config['type'] == 'csv':
+        target = spark.read.csv(target_config['path'], header= target_config['options']['header'],inferSchema=True)
+    elif target_config['type'] == 'json':
+        target = spark.read.json(target_config['path'], multiLine=target_config['options']['multiline'] )
+    elif target_config['type'] == 'parquet':
+        target = spark.read.parquet(target_config['path'])
+    elif target_config['type'] == 'avro':
+        target = spark.read.format('avro').load(target_config['path'])
     return source, target
 
